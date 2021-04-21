ebe42a26c7939b96b5c2767f71bd3e066c2427b4
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-04-20 21:29:10 -0300

    [FLI-106] Test writted

--- app/src/androidTest/java/br/com/raywenderlich/flighter/AirplanesDAOTest.kt
+++ app/src/androidTest/java/br/com/raywenderlich/flighter/AirplanesDAOTest.kt
@@ -1,4 +1,76 @@
 package br.com.raywenderlich.flighter
 
+import android.content.Context
+import android.util.Log
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import br.com.raywenderlich.flighter.dao.AirplaneDAO
+import br.com.raywenderlich.flighter.database.AppDatabase
+import br.com.raywenderlich.flighter.database.data.AirplanesProvider
+import kotlinx.coroutines.runBlocking
+import org.junit.*
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class AirplanesDAOTest {
+
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var database: AppDatabase
+    private lateinit var airplaneDAO: AirplaneDAO
+
+    @Before
+    fun setup() {
+        val context: Context = InstrumentationRegistry.getInstrumentation().context
+        try {
+            database = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+                // Sem essa permissão o Room lança um erro!
+                .allowMainThreadQueries()
+                .build()
+
+        } catch (e: Exception) {
+            e.message?.let { errorMessage ->
+                Log.i(this.javaClass.simpleName, errorMessage)
+            }
+        }
+        airplaneDAO = database.airplaneDAO()
+    }
+
+    @Test
+    fun testInsertAirplanes() {
+        for (airplane in AirplanesProvider.airplanesList) {
+            runBlocking {
+                airplaneDAO.insertAirplane(airplane)
+            }
+        }
+
+        val numberOfAirplanes = airplaneDAO.getAllAirplanes().size
+
+        Assert.assertEquals(2, numberOfAirplanes)
+    }
+
+    @Test
+    fun testClearPassengers() {
+        for (airplane in AirplanesProvider.airplanesList) {
+            runBlocking {
+                airplaneDAO.insertAirplane(airplane)
+            }
+        }
+
+        Log.d("Teste", airplaneDAO.getAllAirplanes().size.toString())
+
+        Assert.assertTrue(airplaneDAO.getAllAirplanes().isNotEmpty())
+
+        airplaneDAO.clearTable()
+        Assert.assertTrue(airplaneDAO.getAllAirplanes().isEmpty())
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
 }
