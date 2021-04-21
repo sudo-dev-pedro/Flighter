51ab51d139594897fa40080e3e32b3375fac59bb
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-04-20 21:31:46 -0300

    [FLI-108] Added data for providers

--- app/src/main/java/br/com/raywenderlich/flighter/database/data/AirplanesProvider.kt
+++ app/src/main/java/br/com/raywenderlich/flighter/database/data/AirplanesProvider.kt
@@ -1,4 +1,30 @@
 package br.com.raywenderlich.flighter.database.data
 
+import br.com.raywenderlich.flighter.database.entity.Airplane
+
 object AirplanesProvider {
+
+    var airplanesList = initAirplanesList()
+
+    private fun initAirplanesList(): MutableList<Airplane> {
+        val airplanes = mutableListOf<Airplane>()
+
+        airplanes.add(
+            Airplane(
+                1,
+                "A330",
+                220
+            )
+        )
+
+        airplanes.add(
+            Airplane(
+                2,
+                "A747",
+                240
+            )
+        )
+
+        return airplanes
+    }
 }
--- app/src/main/java/br/com/raywenderlich/flighter/database/data/FlightsProvider.kt
+++ app/src/main/java/br/com/raywenderlich/flighter/database/data/FlightsProvider.kt
@@ -1,4 +1,54 @@
 package br.com.raywenderlich.flighter.database.data
 
+import br.com.raywenderlich.flighter.database.entity.Flight
+
 object FlightsProvider {
+
+    var flightList = initFlightsList()
+
+    private fun initFlightsList(): MutableList<Flight> {
+        val flights = mutableListOf<Flight>()
+
+        flights.add(
+            Flight(
+                departureCity = "Brasilia",
+                arrivalCity = "Curitiba",
+                departureCityInitials = "BSB",
+                arrivalCityInitials = "CWB",
+                departureDate = "21/04/2021",
+                arrivalDate = "21/04/2021",
+                departureTerminal = 'B',
+                arrivalTerminal = 'A',
+                departureGate = "12",
+                arrivalGate = "22",
+                estimatedFlightTime = "3h 15m",
+                airlineName = "BRAZA",
+                totalPrice = 345.0,
+                airplaneID = 1
+            )
+        )
+
+        // Que horas o voo parte?
+        // Que horas o voo chega?
+        flights.add(
+            Flight(
+                departureCity = "Brasilia",
+                arrivalCity = "Curitiba",
+                departureCityInitials = "BSB",
+                arrivalCityInitials = "CWB",
+                departureDate = "21/04/2021",
+                arrivalDate = "21/04/2021",
+                departureTerminal = 'B',
+                arrivalTerminal = 'A',
+                departureGate = "12",
+                arrivalGate = "22",
+                estimatedFlightTime = "3h 15m",
+                airlineName = "BRAZA",
+                totalPrice = 345.0,
+                airplaneID = 1
+            )
+        )
+
+        return flights
+    }
 }
