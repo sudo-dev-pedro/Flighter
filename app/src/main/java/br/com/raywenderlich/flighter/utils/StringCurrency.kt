36a0a76298f2c50b22ce6eea6a91516cc32825fc
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-05-18 19:33:45 -0300

    [FLI-153] Created Utils folder

---
+++ app/src/main/java/br/com/raywenderlich/flighter/utils/StringCurrency.kt
@@ -0,0 +1,15 @@
+package br.com.raywenderlich.flighter.utils
+
+import java.text.NumberFormat
+import java.util.Locale
+
+class CurrencyFormat {
+    companion object {
+        fun currencyFormat(value: Double): String {
+            val brazilLocale = Locale("pt", "BR")
+            val numberFormat = NumberFormat.getCurrencyInstance(brazilLocale)
+
+            return numberFormat.format(value)
+        }
+    }
+}
