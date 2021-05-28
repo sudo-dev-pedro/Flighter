60593e886f56a15922bc98075115044b73e9917d
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-05-28 18:31:59 -0300

    [FLI-165] Added new module

--- app/src/main/java/br/com/phro/flighter/di/AppModules.kt
+++ app/src/main/java/br/com/phro/flighter/di/AppModules.kt
@@ -1,23 +1,28 @@
 package br.com.phro.flighter.di
 
-import br.com.phro.flighter.FlighterApplication
+import br.com.phro.flighter.FlighterApplication.Companion.database
+import br.com.phro.flighter.repository.AirplaneRepositoryImpl
 import br.com.phro.flighter.repository.FlightRepositoryImpl
 import br.com.phro.flighter.repository.PassengerRepositoryImpl
 import org.koin.dsl.module
 
 val appModule = module {
     single {
-        FlighterApplication.database.passengerDAO()
+        database.passengerDAO()
     }
     single {
-        FlighterApplication.database.flightDAO()
+        database.flightDAO()
     }
-
+    single {
+        database.airplaneDAO()
+    }
     single {
         PassengerRepositoryImpl(passengerDAO = get())
     }
-
     single {
         FlightRepositoryImpl(flightDAO = get())
+    }
+    single {
+        AirplaneRepositoryImpl()
     }
 }
