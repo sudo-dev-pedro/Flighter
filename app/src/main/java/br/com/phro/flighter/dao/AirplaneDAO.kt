98896b6b20658d964d3a114b22f95a882cc77c12
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-06-01 21:12:52 -0300

    [FLI-168] Changed funs to suspend

--- app/src/main/java/br/com/phro/flighter/dao/AirplaneDAO.kt
+++ app/src/main/java/br/com/phro/flighter/dao/AirplaneDAO.kt
@@ -2,23 +2,24 @@ package br.com.phro.flighter.dao
 
 import androidx.room.*
 import br.com.phro.flighter.database.DatabaseConstants
+import br.com.phro.flighter.database.DatabaseConstants.AIRPLANE_TABLE_NAME
 import br.com.phro.flighter.database.entity.Airplane
 
 @Dao
 interface AirplaneDAO {
 
     @Insert
-    fun insertAirplane(airplane: Airplane)
+    suspend fun insertAirplane(airplane: Airplane)
 
-    @Query("SELECT * FROM ${DatabaseConstants.AIRPLANE_TABLE_NAME}")
-    fun getAllAirplanes(): List<Airplane>
+    @Query("SELECT * FROM $AIRPLANE_TABLE_NAME")
+    suspend fun getAllAirplanes(): List<Airplane>
 
     @Update
-    fun updateAirplane(airplane: Airplane)
+    suspend fun updateAirplane(airplane: Airplane)
 
     @Delete
-    fun deleteAirplane(airplane: Airplane)
+    suspend fun deleteAirplane(airplane: Airplane)
 
-    @Query("DELETE FROM ${DatabaseConstants.AIRPLANE_TABLE_NAME}")
-    fun clearTable()
+    @Query("DELETE FROM $AIRPLANE_TABLE_NAME")
+    suspend fun clearTable()
 }
